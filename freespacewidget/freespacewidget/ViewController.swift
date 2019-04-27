6293a48237a4c18000f1d8a6363161f2e7e81f1e
Sergey Vasilevkin <svasilev@mera.ru>
2019-04-27 14:10:07 +0300

    1 - Create new project, iOS Single View App. Set Display name.

---
+++ freespacewidget/freespacewidget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  freespacewidget
+//
+//  Created by Sergey Vasilevkin on 27/04/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
