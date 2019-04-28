acd43caf9d9d97ede9433f014f8fd39060e6a8da
Sergey Vasilevkin <svasilev@mera.ru>
2019-04-28 12:29:16 +0300

    3 - Add a New project Cocoa Touch Framework.

---
+++ FreeSpaceWidgetCore/FreeSpaceWidgetCore/FreeSpaceWidgetCore.h
@@ -0,0 +1,19 @@
+//
+//  FreeSpaceWidgetCore.h
+//  FreeSpaceWidgetCore
+//
+//  Created by Sergey Vasilevkin on 28/04/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FreeSpaceWidgetCore.
+FOUNDATION_EXPORT double FreeSpaceWidgetCoreVersionNumber;
+
+//! Project version string for FreeSpaceWidgetCore.
+FOUNDATION_EXPORT const unsigned char FreeSpaceWidgetCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FreeSpaceWidgetCore/PublicHeader.h>
+
+
